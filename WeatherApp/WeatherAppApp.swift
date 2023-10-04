e11c5ed046fb142f7c1d01dc1d3333336713f5cd
yusuf <yusuf.bak1r3637@gmail.com>
2023-10-04 15:39:48 +0300

    first commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by yusuf bakır on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+q
+
